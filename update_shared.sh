2595952446f6c04cb328918eeb30ee4915f3651d
clorbiecke <144703042+clorbiecke@users.noreply.github.com>
2025-05-21 13:54:10 -0500

    updated update_shared.sh

--- update_shared.sh
+++ update_shared.sh
@@ -10,18 +10,18 @@ echo "Updating shared code from repository..."
 git subtree pull --prefix=$SHARED_PREFIX $SHARED_REPO main --squash
 echo "Pulled latest changes."
 
-# Check if there are changes to commit
-if git status --porcelain | grep "$SHARED_PREFIX"; then
-    echo "Changes detected in shared code. Committing..."
+# # Check if there are changes to commit
+# if git status --porcelain | grep "$SHARED_PREFIX"; then
+#     echo "Changes detected in shared code. Committing..."
     
-    # Add changes and commit
-    git add $SHARED_PREFIX
-    git commit -m "Updated shared code"
+#     # Add changes and commit
+#     git add $SHARED_PREFIX
+#     git commit -m "Updated shared code"
     
-    # Push changes back to the shared repository
-    echo "Pushing changes to shared repository..."
-    git subtree push --prefix=$SHARED_PREFIX $SHARED_REPO main
-    echo "Push completed!"
-else
-    echo "No changes to push."
-fi
+#     # Push changes back to the shared repository
+#     echo "Pushing changes to shared repository..."
+#     git subtree push --prefix=$SHARED_PREFIX $SHARED_REPO main
+#     echo "Push completed!"
+# else
+#     echo "No changes to push."
+# fi
